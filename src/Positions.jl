936aa2b7f2c600b885a5dc918fdfd4ab65c38371
Eric Forgy <Eric.Forgy@coherentasia.com>
2019-02-27 21:39:56 +0800

    Remove Union with Currency

--- src/Positions.jl
+++ src/Positions.jl
@@ -4,18 +4,18 @@ using Reexport
 @reexport using FinancialInstruments
 export Position
 
-struct Position{P<:Union{Currency,FinancialInstrument}}
+struct Position{F<:FinancialInstrument}
     amount::Float64
 end
-Position(p::P,a::Float64) where P<:Union{Currency,FinancialInstrument} = Position{typeof(p)}(a)
+Position(p::FI,a::Float64) where FI<:FinancialInstrument = Position{typeof(p)}(a)
 
-Base.:+(p1::Position{P},p2::Position{P}) where P<:Union{Currency,FinancialInstrument} = Position{P}(p1.amount+p2.amount)
-Base.:-(p1::Position{P},p2::Position{P}) where P<:Union{Currency,FinancialInstrument} = Position{P}(p1.amount-p2.amount)
+Base.:+(p1::Position{FI},p2::Position{FI}) where FI<:FinancialInstrument = Position{FI}(p1.amount+p2.amount)
+Base.:-(p1::Position{FI},p2::Position{FI}) where FI<:FinancialInstrument = Position{FI}(p1.amount-p2.amount)
 
-Base.:/(p1::Position{P},p2::Position{P}) where P<:Union{Currency,FinancialInstrument} = p1.amount/p2.amount
-Base.:/(p::Position{P},k::R) where P<:Union{Currency,FinancialInstrument} where R<:Real = Position{P}(c.amount/k)
+Base.:/(p1::Position{FI},p2::Position{FI}) where FI<:FinancialInstrument = p1.amount/p2.amount
+Base.:/(p::Position{FI},k::R) where FI<:FinancialInstrument where R<:Real = Position{FI}(c.amount/k)
 
-Base.:*(p::Position{P},k::R) where P<:Union{Currency,FinancialInstrument} where R<:Real = Position{P}(p.amount*k)
-Base.:*(k::R,p::Position{P}) where P<:Union{Currency,FinancialInstrument} where R<:Real = p*k
+Base.:*(p::Position{FI},k::R) where FI<:FinancialInstrument where R<:Real = Position{FI}(p.amount*k)
+Base.:*(k::R,p::Position{FI}) where FI<:FinancialInstrument where R<:Real = p*k
 
 end # module
